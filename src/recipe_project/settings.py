679b7b23b6b44a6e45cb8f04ee5db4f860bef33c
Anderson Yu-Hong Cai <113076298+AndersonTsaiTW@users.noreply.github.com>
2025-03-08 00:26:15 -0500

    Update settings.py

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-28n##pn3wf^f0$xc&&2g2^qpo^jw(47$^!ozlqkf97iq7l0%os'
+load_dotenv()
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', secrets.token_urlsafe(50))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
